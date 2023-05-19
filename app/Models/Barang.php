2021b88f9ba32a96658c6c961b3917086cda9a04
mellysujakto <msujakto@gmail.com>
2023-05-19 20:45:38 +0700

    [CHORE] update barang model with fillable and define the created_at and updated_at

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     protected $table = 'barang';
+    protected $fillable = ['id', 'kode_barang', 'nama_barang', 'deskripsi', 'stok_barang', 'harga_barang'];
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
 }
