0679fe10266166b4248da406cb0728a1e11687c9
mellysujakto <msujakto@gmail.com>
2023-05-19 20:58:45 +0700

    [CHORE] update lokasi model with fillable and define the created_at and updated_at

--- app/Models/Lokasi.php
+++ app/Models/Lokasi.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Lokasi extends Model
 {
     protected $table = 'lokasi';
+    protected $fillable = ['id', 'kode_lokasi', 'nama_lokasi'];
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
 }
