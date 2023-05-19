6ea9179d19a7dc89217ce25e998c4256a215534b
mellysujakto <msujakto@gmail.com>
2023-05-19 23:59:52 +0700

    [CHORES] add middleware for kurir table as a user

--- routes/api.php
+++ routes/api.php
@@ -14,26 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['middleware' => 'kurir'], function () {
+    Route::get('/kurir', 'API\KurirController@list');
+    Route::get('/kurir/{id}', 'API\KurirController@getById');
+    Route::post('/kurir', 'API\KurirController@create');
+    Route::put('/kurir', 'API\KurirController@update');
+    Route::delete('/kurir/{id}', 'API\KurirController@delete');
+
+    Route::get('/barang', 'API\BarangController@list');
+    Route::get('/barang/{id}', 'API\BarangController@getById');
+    Route::post('/barang', 'API\BarangController@create');
+    Route::put('/barang', 'API\BarangController@update');
+    Route::delete('/barang/{id}', 'API\BarangController@delete');
+
+    Route::get('/lokasi', 'API\LokasiController@list');
+    Route::get('/lokasi/{id}', 'API\LokasiController@getById');
+    Route::post('/lokasi', 'API\LokasiController@create');
+    Route::put('/lokasi', 'API\LokasiController@update');
+    Route::delete('/lokasi/{id}', 'API\LokasiController@delete');
+
+    Route::post('/pengiriman/input', 'API\PengirimanController@input');
+    Route::post('/pengiriman/approve', 'API\PengirimanController@approve');
 });
-Route::get('/kurir', 'API\KurirController@list');
-Route::get('/kurir/{id}', 'API\KurirController@getById');
-Route::post('/kurir', 'API\KurirController@create');
-Route::put('/kurir', 'API\KurirController@update');
-Route::delete('/kurir/{id}', 'API\KurirController@delete');
-
-Route::get('/barang', 'API\BarangController@list');
-Route::get('/barang/{id}', 'API\BarangController@getById');
-Route::post('/barang', 'API\BarangController@create');
-Route::put('/barang', 'API\BarangController@update');
-Route::delete('/barang/{id}', 'API\BarangController@delete');
-
-Route::get('/lokasi', 'API\LokasiController@list');
-Route::get('/lokasi/{id}', 'API\LokasiController@getById');
-Route::post('/lokasi', 'API\LokasiController@create');
-Route::put('/lokasi', 'API\LokasiController@update');
-Route::delete('/lokasi/{id}', 'API\LokasiController@delete');
-
-Route::post('/pengiriman/input', 'API\PengirimanController@input');
-Route::post('/pengiriman/approve', 'API\PengirimanController@approve');
