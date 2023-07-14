7646fd8bba16ce06cd213ec0f8e17d300f82e01a
mellysujakto <msujakto@gmail.com>
2023-07-14 22:21:08 +0700

    migrate: create migration script for create some tables (barang, pengiriman, lokasi, kurir)

---
+++ database/migrations/2023_07_14_150115_create_pengiriman_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePengirimanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengiriman', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('no_pengiriman', 15);
+            $table->date('tanggal');
+            $table->integer('lokasi_id')->nullable();
+            $table->string('barang_id')->nullable();
+            $table->string('jumlah_barang');
+            $table->string('harga_barang')->nullable();
+            $table->integer('kurir_id')->nullable();
+            $table->boolean('is_approved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengiriman');
+    }
+}
